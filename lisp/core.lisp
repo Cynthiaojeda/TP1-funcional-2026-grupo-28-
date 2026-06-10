eb50613b1b2065a6586ba8a231551ac6cb52312e
Elias <eliastorresj2personal@gmail.com>
2026-06-10 19:19:37 -0300

    Requerimiento 3 y 4

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -6,3 +6,26 @@
 ;; ========================================================================
 (defun registrar-cambio (epoch color-actual cambiar-a)
   (format t "Tiempo ~A: la luz ha cambiado de ~A a ~A~%" epoch color-anterior color-nuevo))
+;; ========================================================================
+;; FUNCIÓN: duracion-ciclo
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Orden Superior 
+;; IMPACTO: No destructiva 
+;; ========================================================================
+(defun duracion-ciclo (tiempos-fase)
+  (reduce #'+ tiempos-fase))
+;; ========================================================================
+;; FUNCIÓN: recomendacion-ciclo
+;; NATURALEZA: Pura 
+;; ESTRATEGIA: Función Predicado / Condicional
+;; IMPACTO: No destructiva
+(defun recomendacion-ciclo (duracion)
+  "Evalúa la duración de un ciclo bajo los estándares de la psicología del conductor (35s - 150s)."
+  (cond
+    ((< duracion 35) 
+     "No recomendado: El ciclo es demasiado corto para la psicología del conductor.")
+    ((> duracion 150) 
+     "No recomendado: El ciclo es demasiado largo (excede los 150s) y genera impaciencia.")
+    (t 
+     "Recomendado: La duración se encuentra en el rango óptimo (35-150 segundos).")))
+
