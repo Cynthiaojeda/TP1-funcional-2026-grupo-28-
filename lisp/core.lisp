90c26bf304046f0c98fe13ff21f1c691c3a48d1d
Elias <eliastorresj2personal@gmail.com>
2026-06-10 19:10:46 -0300

    Requerimiento 3
    Cambio de timestamp por epoch

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,1 +1,8 @@
-;Código del TP1
+
+;; FUNCIÓN: registrar-cambio
+;; NATURALEZA: Impura (Produce efectos secundarios al imprimir en la terminal)
+;; ESTRATEGIA: Directa / Ejecución Secuencial
+;; IMPACTO: No destructiva
+;; ========================================================================
+(defun registrar-cambio (epoch color-actual cambiar-a)
+  (format t "Tiempo ~A: la luz ha cambiado de ~A a ~A~%" epoch color-anterior color-nuevo))
