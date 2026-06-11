615b832505448222e677c26bb5bb8e61a975f61b
Elias <eliastorresj2personal@gmail.com>
2026-06-11 19:34:47 -0300

    función que recomienda la duración de los semaforos

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -64,3 +64,12 @@ REQUERIMIENTO 4b
 ;; ESTRATEGIA: Función Predicado / Condicional
 ;; IMPACTO: No destructiva
 ;; ========================================================================
+(defun recomendacion-ciclo (duracion)
+  "Evalúa la duración de un ciclo bajo los estándares de la psicología del conductor (35s - 150s)."
+  (cond
+    ((< duracion 35) 
+     "No recomendado: El ciclo es demasiado corto para la psicología del conductor.")
+    ((> duracion 150) 
+     "No recomendado: El ciclo es demasiado largo (excede los 150s) y genera impaciencia.")
+    (t 
+     "Recomendado: La duración se encuentra en el rango óptimo (35-150 segundos).")))
