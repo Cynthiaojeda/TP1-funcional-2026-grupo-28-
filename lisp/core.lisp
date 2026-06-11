6372486db3acb094d86899cd1c6894816bc69203
alessandro-srivero <riveroalessandro521@gmail.com>
2026-06-11 03:27:31 -0300

    Requerimiento 5: Calculo directo, uso de Let y Truncate
    Evalué la opción de recursividad pero opté por la simplicidad usando truncate

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,1 +1,12 @@
 ;Código del TP1
+;; ========================================================
+;; FUNCIÓN: ciclos-por-tiempo
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Cálculo Directo
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun ciclos-por-tiempo (minutos rojo amarillo verde)
+  (let ((total (* 60 minutos))
+        (duracion (+ rojo amarillo verde)))
+    (truncate total duracion)))
+
