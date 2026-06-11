9f9fa315430b4957a65eba17945e37948dba38e1
cynthia ojeda <baezroxana1999@gmail.com>
2026-06-11 15:05:57 -0300

    primer commit

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,1 +1,32 @@
-;Código del TP1
+codigo lisp REQUERIMIENTO 1
+;;======================================
+;; FUNCIÓN: Trancisicion 
+;; NATURALEZA: pura 
+;; ESTRATEGIA:
+;; IMPACTO: no destructivo
+;; =======================================
+(defun transicion (color-actual cambiar-a)
+  (cond
+    ((and (equal color-actual 'en-rojo)
+          (equal cambiar-a 'amarillo))
+     (list color-actual "cambiar-a-amarillo"))
+    ((and (equal color-actual 'en-amarillo)
+          (equal cambiar-a 'verde))
+     (list color-actual "cambiar-a-verde"))
+    ((and (equal color-actual 'en-verde)
+          (equal cambiar-a 'rojo))
+     (list color-actual "cambiar-a-rojo"))
+    (t
+     (list color-actual 'accion-por-defecto))))
+
+REQUERIMIENTO 2
+(defun timer (timestamp) " cambiar "
+
+  ; calcular en qué segundo del ciclo estoy
+  (let ((posicion (mod timestamp 216)))
+
+    ; decidir el color según esa posición
+    (cond
+      ((< posicion 90) 'rojo)
+      ((< posicion 96) 'amarillo)
+      (t 'verde))))
