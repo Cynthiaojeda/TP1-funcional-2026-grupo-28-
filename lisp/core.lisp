dd8794743041532469f4bf991e670155188ce859
Elias <eliastorresj2personal@gmail.com>
2026-06-11 18:44:27 -0300

    Función que muestra en pantalla fecha y color del semaforo
    la variable epoch contiene la fecha de tal forma que sea legible para el ser humano

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -38,7 +38,10 @@ REQUERIMIENTO 2
 
 REQUERIMIENTO 3
 ;; ========================================================================
-;; FUNCIÓN: registrar-cambio
+;; FUNCIÓN: registrar-cambio 
 ;; NATURALEZA: Impura 
 ;; ESTRATEGIA: Directa / Ejecución Secuencial
 ;; IMPACTO: No destructiva
+
+(defun registrar-cambio (epoch color-actual cambiar-a)
+  (format t "Tiempo ~A: la luz ha cambiado de ~A a ~A~%" epoch color-anterior color-nuevo))
