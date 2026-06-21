4e3522a952d2bca77b414f16039c7d54207c28d8
Elias <eliastorresj2personal@gmail.com>
2026-06-20 22:34:19 -0300

    ajuste de datos

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -39,7 +39,7 @@
 ;; ESTRATEGIA: Directa / Ejecución Secuencial
 ;; IMPACTO: No destructiva
 (defun registrar-cambio (epoch color-actual cambiar-a)
-  (format t "Tiempo ~A: la luz ha cambiado de ~A a ~A~%" epoch color-anterior color-nuevo))
+  (format t "Tiempo ~A: la luz ha cambiado de ~A a ~A~%" epoch color-actual cambiar-a))
 ;REQUERIMIENTO 4a
 ;; FUNCIÓN: Duración-ciclo
 ;; NATURALEZA: Recursiva 
